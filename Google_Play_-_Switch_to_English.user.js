57b8d69170d540d62932f1e44ba63c416f5bd1ab
Tailszefox <tails@tailszefox.no-ip.com>
2015-07-19 14:19:35 +0200

    Google Play - Force language to English

---
+++ Google_Play_-_Switch_to_English.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Google Play - Switch to English
+// @namespace   localhost
+// @description Forces language to English on Google Play Store
+// @include     https://play.google.com/store/apps/*
+// @exclude     https://play.google.com/store/apps/*hl=en*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var newLocation = window.location.href;
+newLocation = newLocation.replace("&hl=fr", "");
+newLocation = newLocation.replace("?hl=fr", "");
+newLocation += "&hl=en";
+
+window.location.href = newLocation;
