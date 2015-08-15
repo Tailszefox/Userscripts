03ddccc24dcbf6bb6c6929e108fa3ecf5f14af2a
Tailszefox <tails@tailszefox.net>
2015-08-15 10:05:22 +0200

    New script: OVH Manager - fix wrong order on login

---
+++ OVH_-_Fix_2FA.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        OVH - Fix 2FA
+// @namespace   localhost
+// @description Swap the form's order so that Enter works correctly
+// @include     https://www.ovh.com/manager/web/login/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+if(document.querySelector("button[name='otpMethod']") != null)
+{
+    var btn = document.querySelectorAll("button[type='submit']");
+
+    var divBackup = btn[0].parentNode;
+    var divMobile = btn[1].parentNode;
+
+    var htmlTemp = divBackup.innerHTML;
+    divBackup.innerHTML = divMobile.innerHTML;
+    divMobile.innerHTML = htmlTemp;
+}
