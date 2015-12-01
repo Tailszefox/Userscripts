84467f80eabc52ac5d56a333579f3ad497551336
Tailszefox <tails@tailszefox.net>
2015-12-01 21:49:17 +0100

    Twitter_FollowFly: extend include regex

--- Twitter_FollowFly.user.js
+++ Twitter_FollowFly.user.js
@@ -2,7 +2,7 @@
 // @name        Twitter Best
 // @namespace   localhost
 // @description Get best tweets from profile
-// @include     /^https://twitter\.com/[a-zA-Z0-9]+$/
+// @include     /^https://twitter\.com/[a-zA-Z0-9_]+$/
 // @version     1
 // @grant       none
 // ==/UserScript==
