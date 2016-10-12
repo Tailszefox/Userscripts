3244fcd522bd87e892732cbb67b97a26ae49d851
Tailszefox <tails@tailszefox.net>
2016-10-12 17:00:30 +0200

    TwitterFollowFly: Add support for URLs with trailing slash

--- Twitter_FollowFly.user.js
+++ Twitter_FollowFly.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        Twitter Best
+// @name        Twitter - Add link to FollowFly from profile
 // @namespace   localhost
-// @description Get best tweets from profile
-// @include     /^https://twitter\.com/[a-zA-Z0-9_]+$/
+// @description Get best tweets from profile using FollowFly
+// @include     /^https://twitter\.com/[a-zA-Z0-9_]+/?$/
 // @version     1
 // @grant       none
 // ==/UserScript==
@@ -11,6 +11,7 @@ var l = document.createElement("a");
 l.appendChild(document.createTextNode("View on FollowFly"));
 
 var username = window.location.pathname;
+username = username.replace(/\/$/, '');
 l.href = "http://followfly.co/t" + username;
 
 document.querySelector(".ProfileHeaderCard-screenname").appendChild(l);
