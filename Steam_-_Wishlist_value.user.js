ea091fa75dccb34d45a7aec44af04d2137293889
Tailszefox <tails@tailszefox.net>
2018-04-20 20:46:57 +0200

    Steam Wishlist value: fixed for new wishlist design

--- Steam_-_Wishlist_value.user.js
+++ Steam_-_Wishlist_value.user.js
@@ -4,9 +4,9 @@
 // @namespace   localhost
 // @description How much would it cost to buy all the games in your wishlist?
 // @icon        https://i.imgur.com/GybYj5c.png
-// @include     https://steamcommunity.com/id/tailszefox/wishlist
-// @include     https://steamcommunity.com/id/tailszefox/wishlist/
-// @include     https://steamcommunity.com/id/tailszefox/wishlist/*
+// @include     http://store.steampowered.com/wishlist/profiles/76561197967038647
+// @include     http://store.steampowered.com/wishlist/profiles/76561197967038647/
+// @include     http://store.steampowered.com/wishlist/profiles/76561197967038647/*
 // @version     1
 // @grant       none
 // ==/UserScript==
@@ -25,26 +25,28 @@ function calculateWishlistValue()
 {
     this.childNodes[0].nodeValue = "Calculating...";
 
-    var games = document.querySelectorAll(".wishlistRow");
+    var games = document.querySelectorAll(".wishlist_row");
     var totalFull = 0;
     var totalDiscount = 0;
 
-    games.forEach(function(game) {
-        // Game is not on sale
-        if ( game.querySelector(".price") )
-        {
-            p = extractPrice(game.querySelector(".price"));
-            totalFull += p;
-            totalDiscount += p;
-        }
-
+    for (var gameId in g_Wishlist.rgElements)
+    {
+        var game = g_Wishlist.rgElements[gameId][0];
+      
         // Game is on sale
-        else if ( game.querySelector(".discount_prices") )
+        if ( game.querySelector(".discount_original_price") )
         {
             totalFull += extractPrice(game.querySelector(".discount_original_price"));
             totalDiscount += extractPrice(game.querySelector(".discount_final_price"));
         }
-    });
+        // Game is not on sale
+        else if ( game.querySelector(".discount_final_price") )
+        {
+            p = extractPrice(game.querySelector(".discount_final_price"));
+            totalFull += p;
+            totalDiscount += p;
+        }
+    }
 
     this.childNodes[0].nodeValue = "Wishlist value: "+ totalDiscount.toFixed(2) +"€ (prices on sale) / "+ totalFull.toFixed(2) +"€ (regular prices)";
 }
@@ -57,7 +59,7 @@ divBtn.style.marginBottom = "5px";
 divBtn.className = "btnv6_blue_hoverfade btn_small";
 divBtn.appendChild(spanBtn);
 
-var wishlistItems = document.querySelector("#wishlist_items");
-wishlistItems.parentNode.insertBefore(divBtn, wishlistItems);
+var wishlistDiv = document.querySelector("#wishlist_ctn");
+wishlistDiv.parentNode.insertBefore(divBtn, wishlistDiv);
 
 spanBtn.addEventListener("click", calculateWishlistValue, false);
