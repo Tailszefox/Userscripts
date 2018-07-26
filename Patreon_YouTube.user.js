9050507b6f907fae94f6e6d0a5ae47717595946d
Tailszefox <tails@tailszefox.net>
2018-07-26 21:29:01 +0200

    New script: Patreon YouTube
    Adds a link below YouTube videos on Patreon posts
    so you don't have to start playing them

---
+++ Patreon_YouTube.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        Patreon YouTube
+// @author      Tailszefox
+// @namespace   localhost
+// @description Adds a link to YouTube videos on Patreon posts
+// @include     https://www.patreon.com/posts/*
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+if( window.patreon.bootstrap.post.data.attributes.embed )
+{
+  // Embed available
+  var youtubeUrl = window.patreon.bootstrap.post.data.attributes.embed.url;
+  
+  var divLink = document.createElement("div");
+  divLink.style.margin = "5px";
+  
+  var aLink = document.createElement("a");
+  aLink.href = youtubeUrl;
+  
+  var txtNode = document.createTextNode(youtubeUrl);
+  aLink.appendChild(txtNode);
+  divLink.appendChild(aLink);
+  
+  var titleNode = document.querySelector(".ejBwtx");
+  titleNode.parentNode.insertBefore(divLink, titleNode);
+}
