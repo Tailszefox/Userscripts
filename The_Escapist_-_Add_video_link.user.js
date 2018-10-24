f906e26829d3dd98646e2cd54edb9cd3765d0842
Tailszefox <tails@tailszefox.net>
2018-10-24 23:57:28 +0200

    Escapist: new design

--- The_Escapist_-_Add_video_link.user.js
+++ The_Escapist_-_Add_video_link.user.js
@@ -5,6 +5,7 @@
 // @description Adds a direct link to the video
 // @icon        https://i.imgur.com/TCs4Wnm.png
 // @include     http://www.escapistmagazine.com/videos/view/*
+// @include     https://www.escapistmagazine.com/v2/*
 // @version     1
 // @grant       none
 // ==/UserScript==
@@ -24,9 +25,12 @@ function addLink()
     link.appendChild(document.createTextNode("Direct link"));
     link.className = "video_menu_link";
     link.href = video.src;
+  
+    var divider = document.querySelector(".dot-divider").cloneNode(true);
 
-    var menu = document.querySelector("#video_player_menu");
-    menu.insertBefore(link, menu.children[0]);
+    var menu = document.querySelector(".meta-container");
+    menu.appendChild(divider);
+    menu.appendChild(link);
 }
 
 window.setTimeout(addLink, 5000);
