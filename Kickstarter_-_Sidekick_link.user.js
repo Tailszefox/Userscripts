0b4490c98d9ce2eea2db7c220fe0d49ef856a5ab
Tailszefox <tails@tailszefox.net>
2019-01-28 06:45:08 +0100

    Kickstarter: changed sidekick to kicktraq

--- Kickstarter_-_Sidekick_link.user.js
+++ Kickstarter_-_Sidekick_link.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        Kickstarter - Sidekick link
+// @name        Kickstarter - Kicktraq link
 // @author      Tailszefox
 // @namespace   localhost
-// @description Adds a Sidekick link to Kickstarter projects
+// @description Adds a Kicktraq link to Kickstarter projects
 // @icon        https://i.imgur.com/cK0HSWf.png
 // @include     https://www.kickstarter.com/projects/*
 // @version     1
@@ -14,17 +14,17 @@ window.addEventListener("load", function(){
         // If the project is live
         if( document.querySelector("#main_content").className == "Campaign-state-live" )
         {
-            var projectId = document.querySelector("data[itemprop='Project[comments_count]']").className.match(/Project([0-9]+)/);
+            var projectUrl = encodeURIComponent(window.location.href);
 
             var div = document.createElement("div");
             var link = document.createElement("a");
             div.appendChild(link);
 
-            link.appendChild(document.createTextNode("View Sidekick page"));
-            link.href = "http://sidekick.epfl.ch/campaign/" + projectId[1] +  "-project";
+            link.appendChild(document.createTextNode("View Kicktraq page"));
+            link.href = "https://www.kicktraq.com/search/?find=" + projectUrl;
             div.style.marginTop = "20px";
 
-            document.querySelector(".NS_campaigns__stats").parentNode.insertBefore(div, null);
+            document.querySelectorAll("#react-project-header > div > div > div > div > div")[5].parentNode.insertBefore(div, null);
         }
     }, 5000);
 });
