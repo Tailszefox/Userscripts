8c3160273d1b985c68d0157ca24636df4f4b2eaf
Tailszefox <tails@tailszefox.net>
2019-03-12 04:00:50 +0100

    New script: change layout for Numerama

---
+++ Numerama_-_Change_layout.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        Numerama - Change layout
+// @author      Tailszefox
+// @namespace   localhost
+// @description Change layout on front page of Numerama because the button to do it doesn't work
+// @match       https://www.numerama.com/
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+window.setTimeout(function(){
+  var sortButton = document.querySelector(".sort-list-format");
+
+  var n = $(sortButton);
+  var i = n.getAjaxContainer();
+  var a = i.getConfigContainer(n, i);
+  var t = i.showLoader();
+
+  $.ajax(
+    {
+      type: 'GET',
+      url: "https://www.numerama.com/ajax.php",
+      data: a,
+      dataType: 'JSON',
+      success: function (a) {
+        t.endLoading(),
+        void 0 !== a.html && i.html(a.html),
+        i.data('ajax-id') && (a.container = i.data('ajax-id')),
+        NUM.ajaxContentTriggers(),
+        i.animate({
+          opacity: 1
+        }, 200, function () {
+          console.log("Done");
+        })
+      }
+    }
+  );
+}, 5000);
