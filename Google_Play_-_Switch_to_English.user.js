ba554b6e4e11ba3e776258e0468573b8c9e35ab5
Tailszefox <tails@tailszefox.net>
2019-03-24 04:20:06 +0100

    Google Play: add a link to change language instead of switching automatically

--- Google_Play_-_Switch_to_English.user.js
+++ Google_Play_-_Switch_to_English.user.js
@@ -2,11 +2,11 @@
 // @name        Google Play - Switch to English
 // @author      Tailszefox
 // @namespace   localhost
-// @description Forces language to English on Google Play Store
+// @description Add a link to switch to English on Google Play Store
 // @icon        https://i.imgur.com/7RZiSjY.jpg
 // @include     https://play.google.com/store/*
 // @exclude     https://play.google.com/store/*hl=en*
-// @version     1.0
+// @version     1.1
 // @grant       none
 // @inject-into content
 // ==/UserScript==
@@ -16,4 +16,13 @@ newLocation = newLocation.replace("&hl=fr", "");
 newLocation = newLocation.replace("?hl=fr", "");
 newLocation += "&hl=en";
 
-window.location.href = newLocation;
+var changeList = document.createElement("li");
+changeList.classList.add("fle8Af");
+
+var changeLink = document.createElement("a");
+changeLink.classList.add("gm1qLe");
+changeLink.href = newLocation;
+
+changeLink.appendChild(document.createTextNode("English"));
+changeList.appendChild(changeLink);
+document.querySelectorAll("ul.CoKTif")[0].appendChild(changeList);
